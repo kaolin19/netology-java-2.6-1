7679164e67aa097c87b6fea599bf4e31cee091b4
Boris Jarczuk <yarchuk.b@gmail.com>
2021-03-23 21:13:41 +0300

    test methods fixed

--- src/test/java/ru/netology/radio/RadioTest.java
+++ src/test/java/ru/netology/radio/RadioTest.java
@@ -9,107 +9,93 @@ class RadioTest {
     Radio radio = new Radio();
 
     @Test
-    void shouldSetCurrentStation() {
-        int station = 0;
-        radio.setCurrentStation(station);
-        assertEquals(0, radio.getCurrentStation());
-
-        station = 9;
-        radio.setCurrentStation(station);
-        assertEquals(9, radio.getCurrentStation());
-
-        station = -1;
-        radio.setCurrentStation(station);
-        assertEquals(9, radio.getCurrentStation());
-
-        station = 1;
-        radio.setCurrentStation(station);
-        assertEquals(1, radio.getCurrentStation());
-
-        station = 10;
-        radio.setCurrentStation(station);
-        assertEquals(1, radio.getCurrentStation());
-
-        station = 8;
+    void shouldSetCurrentStationWithinRange() {
+        int station = 8;
         radio.setCurrentStation(station);
         assertEquals(8, radio.getCurrentStation());
     }
 
     @Test
-    void shouldSetCurrentVolume() {
-        int volume = 0;
-        radio.setCurrentVolume(volume);
-        assertEquals(0, radio.getCurrentVolume());
+    void shouldSetCurrentStationOutOfRange() {
+        int station = 10;
+        radio.setCurrentStation(station);
+        assertEquals(0, radio.getCurrentStation());
+    }
 
-        volume = -1;
-        radio.setCurrentVolume(volume);
-        assertEquals(0, radio.getCurrentVolume());
-
-
-        volume = 1;
-        radio.setCurrentVolume(volume);
-        assertEquals(1, radio.getCurrentVolume());
-
-        volume = 11;
-        radio.setCurrentVolume(volume);
-        assertEquals(1, radio.getCurrentVolume());
-
-        volume = 10;
-        radio.setCurrentVolume(volume);
-        assertEquals(10, radio.getCurrentVolume());
-
-        volume = 9;
+    @Test
+    void shouldSetCurrentVolumeWithinRange() {
+        int volume = 9;
         radio.setCurrentVolume(volume);
         assertEquals(9, radio.getCurrentVolume());
     }
 
     @Test
-    void shouldSetPrevStation() {
+    void shouldSetCurrentVolumeOutOfRange() {
+        int volume = 11;
+        radio.setCurrentVolume(volume);
+        assertEquals(0, radio.getCurrentVolume());
+
+    }
+
+    @Test
+    void shouldSetPrevStationWithinRange() {
         int station = 1;
         radio.setCurrentStation(station);
         radio.prevStation();
         assertEquals(0,radio.getCurrentStation());
+    }
 
-        station = 0;
+    @Test
+    void shouldSetPrevStationOutOfRange() {
+        int station = 0;
         radio.setCurrentStation(station);
         radio.prevStation();
         assertEquals(9,radio.getCurrentStation());
     }
 
     @Test
-    void shouldSetNextStation() {
+    void shouldSetNextStationWithinRange() {
         int station = 8;
         radio.setCurrentStation(station);
         radio.nextStation();
         assertEquals(9,radio.getCurrentStation());
+    }
 
-        station = 9;
+    @Test
+    void shouldSetNextStationOutOfRange() {
+        int station = 9;
         radio.setCurrentStation(station);
         radio.nextStation();
         assertEquals(0,radio.getCurrentStation());
     }
 
     @Test
-    void shouldSetMinusVolume() {
+    void shouldSetMinusVolumeWithinRange() {
         int volume = 1;
-        radio.setCurrentVolume(volume);
-        radio.minusVolume();
-        assertEquals(0,radio.getCurrentVolume());
-
-        volume = 0;
         radio.setCurrentVolume(volume);
         radio.minusVolume();
         assertEquals(0,radio.getCurrentVolume());
     }
 
     @Test
-    void shouldSetPlusVolume() {
+    void shouldSetMinusVolumeOutOfRange() {
+        int volume = 0;
+        radio.setCurrentVolume(volume);
+        radio.minusVolume();
+        assertEquals(0,radio.getCurrentVolume());
+    }
+
+    @Test
+    void shouldSetPlusVolumeWithinRange() {
         int volume = 9;
         radio.setCurrentVolume(volume);
         radio.plusVolume();
         assertEquals(10,radio.getCurrentVolume());
+    }
 
-        volume = 10;
+    @Test
+    void shouldSetPlusVolumeOutOfRange() {
+        int volume = 10;
         radio.setCurrentVolume(volume);
         radio.plusVolume();
         assertEquals(10,radio.getCurrentVolume());
